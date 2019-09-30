c445b850c928c547082fdb9c3c28a52cf75544d5
Jesse Zhang <XZhang@lcs.on.ca>
2019-09-30 11:09:53 -0400

    Initial Commit

---
+++ Guessing Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guessing Game
+//
+//  Created by Zhang, Jesse on 2019-09-30.
+//  Copyright © 2019 Zhang, Jesse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
